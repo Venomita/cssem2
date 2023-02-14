85c28886520581d98a11bdd7c08601d7f838a5cc
Hamza Paracha <parachaham@gmail.com>
2023-02-14 21:32:48 +0000

    setting up the environment

---
+++ program2.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Welcome to Hamza's Apple store!" +
+        "Here, we sell Apples for $2.50 each. ");
+        Console.Write("How many Apples would you like to purchase?");
+        int quantity = Console.ReadLine();
+        int subtotal
+
+
+    }
+}
