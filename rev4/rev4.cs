7baea17bef609b616c15837b6bc77316f24180ee
Hamza Paracha <parachaham@gmail.com>
2023-02-19 17:15:46 +0000

    work

--- rev4/rev4.cs
+++ rev4/rev4.cs
@@ -1,7 +1,21 @@
 using System;
 class Program3
 {
-    static void Main(string[] args){
-        
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of antennas that were sighted. ");
+        String numofantennas = Console.ReadLine();
+        int numofantennasF;
+        int.TryParse(numofantennas, out numofantennasF);
+        Console.WriteLine("Enter the number of eyes that were sighted. ");
+        String numofeyes = Console.ReadLine();
+        int numofeyesF;
+        int.TryParse(numofeyes, out numofeyesF);
+        if (numofeyesF > 3 && numofeyesF < 6)
+        {
+
+        }
+
+        Console.ReadKey();
     }
 }
